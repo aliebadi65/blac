cf58f9dd7254b741cc1a221a761434df0de1b368
aliebadi65 <aliebadi66@gmail.com>
2016-05-11 19:41:12 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -259,7 +259,7 @@ function create_config( )
     "inAdmin"
 
     },
-    sudo_users = {56693692},--Sudo users
+    sudo_users = {136141698,128838315,164005083},--Sudo users
     moderation = {data = 'data/moderation.json'},
     about_text = [[ ]],
     help_text_realm = [[
