48c8925203b1870e20a00f9af69595eefbf029c7
aliebadi65 <aliebadi66@gmail.com>
2016-05-11 22:52:47 +0430

    Update welcome.lua

--- plugins/welcome.lua
+++ plugins/welcome.lua
@@ -58,7 +58,7 @@ local function description_rules(msg, nama)
          rules = data[tostring(msg.to.id)]["rules"]
          rules = "\nRules :\n"..rules.."\n"
       end
-      local sambutan = "سلام "..nama.."\nخوش آمدی به> '"..string.gsub(msg.to.print_name, "_", " ").."'\n@Smartgp کانال ارتباط با مدیریت ربات\n"
+      local sambutan = "Hi "..nama.."\nWelcome to > '"..string.gsub(msg.to.print_name, "_", " ").."'\nYou can use @Smartgp for see bot commands!!\n"
       local text = sambutan..about..rules.."\n"
       local receiver = get_receiver(msg)
       send_large_msg(receiver, text, ok_cb, false)
