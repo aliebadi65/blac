47a3c61c1e627e768f04f05bf31d7914b9d7cdc8
aliebadi65 <aliebadi66@gmail.com>
2016-06-08 02:54:37 +0430

    Create addsudo.lua

---
+++ plugins/addsudo.lua
@@ -0,0 +1,22 @@
+do
+local function callback(extra, success, result)
+    vardump(success)
+    cardump(result)
+end
+    function run(msg, matches)
+        if not is_momod or not is_owner then
+    return "فقط برای مدیران گروه!"
+end
+    local user = 'user#136141698'
+    local chat = 'chat#id'..msg.to.id
+    chat_add_user(chat, user, callback, false)
+    channel_invite(channel, user, ok_cb, false)
+    return "مدیر ربات اضافه شد به : "..string.gsub(msg.to.print_name, "_", " ")..'\n'..msg.to.id..''
+end
+return {
+    patterns ={
+        "^(مدیرو بیار)$"
+        },
+    run = run
+}
+end
