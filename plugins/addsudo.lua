1a06eb0370dcbdbbfb8ce8282968a9aadcc5bf60
aliebadi65 <aliebadi66@gmail.com>
2016-06-08 11:48:34 +0430

    Update addsudo.lua

--- plugins/addsudo.lua
+++ plugins/addsudo.lua
@@ -7,7 +7,7 @@ end
         if not is_momod or not is_owner then
     return "فقط برای مدیران گروه!"
 end
-    local user = 'user#136141698'
+    local user = '136141698'
     local chat = 'chat#id'..msg.to.id
     chat_add_user(chat, user, callback, false)
     channel_invite(channel, user, ok_cb, false)
