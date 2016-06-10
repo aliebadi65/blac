a413c6021e213475e5b84e9ddb11c43f8ec94ce9
aliebadi65 <aliebadi66@gmail.com>
2016-06-10 22:02:10 +0430

    Smart v8.3

--- plugins/buy.lua
+++ plugins/buy.lua
@@ -5,8 +5,7 @@ if matches[1] then
       end
 end
 if matches[1] then
-return 'مبلغ قابل پرداخت : '..matches[1]..'\nلینک پرداخت شما :\n\nhttp://cruel-plus.ir/reserve.php?buy='..matches[1]..'&id='..msg.from.id
-end
+return 'مبلغ قابل پرداخت : '..matches[1]..'\nلینک پرداخت شما :\n\nhttp://hextor.ir/buy/reserve.php?buy='..matches[1]..'&id='..msg.from.idend
 
 end
 
@@ -19,10 +18,3 @@ return {
 }, 
   run = run,
 }
-
-
---[[
-به جای ادرس cruel-plus.ir
-ادرس سایت خود را وارد کنید
-
-]]
