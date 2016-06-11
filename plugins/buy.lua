765cd93b4ec34afbc81fe6b00ce9fb3e5e346dc9
aliebadi65 <aliebadi66@gmail.com>
2016-06-11 14:47:06 +0430

    Update buy.lua

--- plugins/buy.lua
+++ plugins/buy.lua
@@ -5,9 +5,7 @@ if matches[1] then
       end
 end
 if matches[1] then
-return 'مبلغ قابل پرداخت : '..matches[1]..'\nلینک پرداخت شما :\n\nhttp://cruel-plus.ir/reserve.php?buy='..matches[1]..'&id='..msg.from.id
-end
-
+return 'مبلغ قابل پرداخت : '..matches[1]..'\nلینک پرداخت شما :\n\nhttp://hextor.ir/buy/reserve.php?buy='..matches[1]..'&id='..msg.from.id
 end
 
 return {
