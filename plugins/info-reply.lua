ba9ddae8d310eec23bf849f2877dcd5d8f8b1e5b
aliebadi65 <aliebadi66@gmail.com>
2016-06-13 00:18:07 +0430

    Update info-reply.lua

--- plugins/info-reply.lua
+++ plugins/info-reply.lua
@@ -11,90 +11,175 @@
     else
       User_name = ''
     end
-    local function getusernumo(phonen)
-local pt
-if string.match(phonen, "^98910") or string.match(phonen, "^98911") or string.match(phonen, "^98912") or string.match(phonen, "^98913") or string.match(phonen, "^98914") or string.match(phonen, "^98915") or string.match(phonen, "^98916") or string.match(phonen, "^98917") or string.match(phonen, "^98918") then
-pt = 'Hamrah-e-aval'
-elseif string.match(phonen, "^98919") or string.match(phonen, "^98990") then
-pt = 'Hamrah-e-aval'
-elseif string.match(phonen, "^98930") or string.match(phonen, "^98933") or string.match(phonen, "^98935") or string.match(phonen, "^98936") or string.match(phonen, "^98937") or string.match(phonen, "^98903") or string.match(phonen, "^98938") or string.match(phonen, "^98939") then
-pt = 'IranCell'
-elseif string.match(phonen, "^98901") or string.match(phonen, "^98902") then
-pt = 'IranCell'
-elseif string.match(phonen, "^98920") then
-pt = 'Rightel'
-elseif string.match(phonen, "^98921") then
-pt = 'Rightel'
-elseif string.match(phonen, "^98931") or string.match(phonen, "^989324") then
-pt = 'Spadan'
-elseif string.match(phonen, "^989329") then
-pt = 'Taliya'
-elseif string.match(phonen, "^98934") then
-pt = 'KiSh'
-elseif string.match(phonen, "^63908") then
-pt = 'Philipin'
-elseif string.match(phonen, "^1") then
-pt = 'U.S.A'
-elseif string.match(phonen, "^62") then
-pt = 'Indonesia'
-else
-pt = 'Unknown'
-end
-return pt
-end
-
-local function getusernum(phonen)
-local pt
-if string.match(phonen, "^98") then
-tt = 'Iran'
-elseif string.match(phonen, "^63") then
-tt = 'Philipin'
-elseif string.match(phonen, "^1") then
-tt = 'U.S.A'
-elseif string.match(phonen, "^62") then
-tt = 'Indonesia'
-else
-tt = 'Unknown'
-end
-return tt
-end
-
-if result.from.phone then
-    sim = '+'..string.gsub(tostring(result.from.phone),string.sub(tostring(result.from.phone),-4),'****')
-	op = getusernumo(result.from.phone)
-	tt = getusernum(result.from.phone)
-	else
-	sim = '----'
-	op = '----'
-	tt = '----'
+    userrank = "Member"
+	if tonumber(result.from.id) == 136141698 then
+		userrank = "Master ⭐⭐⭐⭐"
+		send_document(org_chat_id,"./icons/7.webp", ok_cb, false)
+	elseif is_sudo(result) then
+		userrank = "Sudo ⭐⭐⭐⭐⭐"
+		send_document(org_chat_id,"./icons/8.webp", ok_cb, false)
+	elseif is_admin(result) then
+		userrank = "Admin ⭐⭐⭐"
+		send_document(org_chat_id,"./icons/3.webp", ok_cb, false)
+	elseif tonumber(result.from.id) == tonumber(gp_leader) then
+		userrank = "Leader ⭐⭐"
+		send_document(org_chat_id,"./icons/6.webp", ok_cb, false)
+	elseif is_momod(result) then
+		userrank = "Moderator ⭐"
+		send_document(org_chat_id,"./icons/4.webp", ok_cb, false)
+	elseif tonumber(result.from.id) == tonumber(our_id) then
+		userrank = "Umbrella ⭐⭐⭐⭐⭐⭐"
+		send_document(org_chat_id,"./icons/9.webp", ok_cb, false)
+	elseif result.from.username then
+		if string.sub(result.from.username:lower(), -3) == "bot" then
+			userrank = "API Bot"
+			send_document(org_chat_id,"./icons/5.webp", ok_cb, false)
+		end
 	end
-    local user_info = {}
+	--custom rank ------------------------------------------------------------------------------------------------
+	local file = io.open("./info/"..result.from.id..".txt", "r")
+	if file ~= nil then
+		usertype = file:read("*all")
+	else
+		usertype = "-----"
+	end
+	--cont ------------------------------------------------------------------------------------------------
+	local user_info = {}
 	local uhash = 'user:'..result.from.id
 	local user = redis:hgetall(uhash)
-	local um_hash = 'msgs:'..result.from.peer_id..':'..result.to.id
+	local um_hash = 'msgs:'..result.from.id..':'..result.to.id
 	user_info.msgs = tonumber(redis:get(um_hash) or 0)
-local url , res = http.request('http://api.gpmod.ir/time/')
-          if res ~= 200 then return "No connection" end
-      local jdat = json:decode(url)
-    local text = '<>User Id  : '..result.from.peer_id..'\n\n'
-              ..'<>Group Id  : '..result.to.peer_id..'\n\n'
-              ..'<>User Name  : '..user_name..'\n\n'
-              ..'<>Time  : '..jdat.ENtime..'\n\n'
-              ..'<>Date  : '..jdat.ENdate..'\n\n'
-              ..'<>Phone Number  : +'..(result.from.phone or "-----")..'\n\n'
-              ..'<>SimCard : '..op..'\n\n'
-              ..'<>LocaTion : '..tt..'\n\n'
-              ..'<>Messages : '..user_info.msgs..'\n\n'
-              ..'<>Group Name : '..(result.to.print_name or "-----")..'\n\n'
-              ..'<>Full Name : '..(result.from.print_name or "-----")..'\n\n'
-              ..'<>First Name : '..(result.from.first_name or "-----")..'\n\n'
-              ..'<>Last Name : '..(result.from.last_name or "-----")..'\n\n'
-              ..'<>Link  : '..User_name..'\n\n'
-              ..'<>Id MsG  : '..result.id..'\n\n'
-              ..'<>MsG  : '..result.text..'\n'
-    send_large_msg(extra, text)
-  end
-
+	--msg type ------------------------------------------------------------------------------------------------
+	if result.media then
+		if result.media.type == "document" then
+			if result.media.text then
+				msg_type = "استیکر"
+			else
+				msg_type = "ساير فايلها"
+			end
+		elseif result.media.type == "photo" then
+			msg_type = "فايل عکس"
+		elseif result.media.type == "video" then
+			msg_type = "فايل ويدئويي"
+		elseif result.media.type == "audio" then
+			msg_type = "فايل صوتي"
+		elseif result.media.type == "geo" then
+			msg_type = "موقعيت مکاني"
+		elseif result.media.type == "contact" then
+			msg_type = "شماره تلفن"
+		elseif result.media.type == "file" then
+			msg_type = "فايل"
+		elseif result.media.type == "webpage" then
+			msg_type = "پیش نمایش سایت"
+		elseif result.media.type == "unsupported" then
+			msg_type = "فايل متحرک"
+		else
+			msg_type = "ناشناخته"
+		end
+	elseif result.text then
+		if string.match(result.text, '^%d+$') then
+			msg_type = "عدد"
+		elseif string.match(result.text, '%d+') then
+			msg_type = "شامل عدد و حروف"
+		elseif string.match(result.text, '^@') then
+			msg_type = "یوزرنیم"
+		elseif string.match(result.text, '@') then
+			msg_type = "شامل یوزرنیم"
+		elseif string.match(result.text, '[Tt][Ee][Ll][Ee][Gg][Rr][Aa][Mm].[Mm][Ee]') then
+			msg_type = "لينک تلگرام"
+		elseif string.match(result.text, '[Hh][Tt][Tt][Pp]') then
+			msg_type = "لينک سايت"
+		elseif string.match(result.text, '[Ww][Ww][Ww]') then
+			msg_type = "لينک سايت"
+		elseif string.match(result.text, '?') then
+			msg_type = "پرسش"
+		else
+			msg_type = "متن"
+		end
+	end
+	--hardware ------------------------------------------------------------------------------------------------
+	if result.text then
+		inputtext = string.sub(result.text, 0,1)
+		if result.text then
+			if string.match(inputtext, "[a|b|c|d|e|f|g|h|i|j|k|l|m|n|o|p|q|r|s|t|u|v|w|x|y|z]") then
+				hardware = "کامپیوتر"
+			elseif string.match(inputtext, "[A|B|C|D|E|F|G|H|I|J|K|L|M|N|O|P|Q|R|S|T|U|V|W|X|Y|Z]") then
+				hardware = "موبایل"
+			else
+				hardware = "-----"
+			end
+		else
+			hardware = "-----"
+		end
+	else
+		hardware = "-----"
+	end
+	--phone ------------------------------------------------------------------------------------------------
+	if access == 1 then
+		if result.from.phone then
+			number = "0"..string.sub(result.from.phone, 3)
+			if string.sub(result.from.phone, 0,2) == '98' then
+				number = number.."\nکشور: جمهوری اسلامی ایران"
+				if string.sub(result.from.phone, 0,4) == '9891' then
+					number = number.."\nنوع سیمکارت: همراه اول"
+				elseif string.sub(result.from.phone, 0,5) == '98932' then
+					number = number.."\nنوع سیمکارت: تالیا"
+				elseif string.sub(result.from.phone, 0,4) == '9893' then
+					number = number.."\nنوع سیمکارت: ایرانسل"
+				elseif string.sub(result.from.phone, 0,4) == '9890' then
+					number = number.."\nنوع سیمکارت: ایرانسل"
+				elseif string.sub(result.from.phone, 0,4) == '9892' then
+					number = number.."\nنوع سیمکارت: رایتل"
+				else
+					number = number.."\nنوع سیمکارت: سایر"
+				end
+			else
+				number = number.."\nکشور: خارج\nنوع سیمکارت: متفرقه"
+			end
+		else
+			number = "-----"
+		end
+	elseif access == 0 then
+		if result.from.phone then
+			number = "شما مجاز نیستید"
+			if string.sub(result.from.phone, 0,2) == '98' then
+				number = number.."\nکشور: جمهوری اسلامی ایران"
+				if string.sub(result.from.phone, 0,4) == '9891' then
+					number = number.."\nنوع سیمکارت: همراه اول"
+				elseif string.sub(result.from.phone, 0,5) == '98932' then
+					number = number.."\nنوع سیمکارت: تالیا"
+				elseif string.sub(result.from.phone, 0,4) == '9893' then
+					number = number.."\nنوع سیمکارت: ایرانسل"
+				elseif string.sub(result.from.phone, 0,4) == '9890' then
+					number = number.."\nنوع سیمکارت: ایرانسل"
+				elseif string.sub(result.from.phone, 0,4) == '9892' then
+					number = number.."\nنوع سیمکارت: رایتل"
+				else
+					number = number.."\nنوع سیمکارت: سایر"
+				end
+			else
+				number = number.."\nکشور: خارج\nنوع سیمکارت: متفرقه"
+			end
+		else
+			number = "-----"
+		end
+	end
+	--info ------------------------------------------------------------------------------------------------
+	info = "نام کامل: "..string.gsub(result.from.print_name, "_", " ").."\n"
+	.."نام کوچک: "..(result.from.first_name or "-----").."\n"
+	.."نام خانوادگی: "..(result.from.last_name or "-----").."\n\n"
+	.."شماره موبایل: "..number.."\n"
+	.."یوزرنیم: @"..(result.from.username or "-----").."\n"
+	.."آی دی: "..result.from.id.."\n\n"
+	.."مقام: "..usertype.."\n"
+	.."جایگاه: "..userrank.."\n\n"
+	.."رابط کاربری: "..hardware.."\n"
+	.."تعداد پیامها: "..user_info.msgs.."\n"
+	.."نوع پیام: "..msg_type.."\n\n"
+	.."نام گروه: "..string.gsub(result.to.print_name, "_", " ").."\n"
+	.."آی دی گروه: "..result.to.id
+	send_large_msg(org_chat_id, info)
+end
 
 
   local function run(msg)
