1ccc6d899bc024ba8a40bea93ca50e713e8bb42e
aliebadi65 <aliebadi66@gmail.com>
2016-06-14 23:36:37 +0430

    Update warn.lua

--- plugins/warn.lua
+++ plugins/warn.lua
@@ -30,7 +30,7 @@ local function warn_by_username(extra, success, result) -- /warn <@username>
    local hash =  'banned:'..target
    redis:sadd(hash, msg.id)
   text = '[ '..name..' ]\n به دلیل رعایت نکردن قوانین از گروه اخراج شد (banned)\nتعداد اخطار ها : ۴/۴'
-  channel_kick_user(receiver, 'user#id'..msg.id, ok_cb, false)
+  kick_user(msg.from.id, msg.to.id)
    end
   else
    redis:hset(hash, msg.id, '1')
@@ -75,7 +75,7 @@ local function warn_by_reply(extra, success, result) -- (on reply) /warn
   text = '[ '..name..' ]\n به دلیل رعایت نکردن قوانین از گروه اخراج شد. (banned)\nتعداد اخطار ها : ۴/۴'
   local hash =  'banned:'..target
   redis:sadd(hash, msg.from.id)
-   channel_kick_user(receiver, 'user#id'..msg.id, ok_cb, false)
+  kick_user(msg.from.id, msg.to.id)
    end
   else
    redis:hset(hash, msg.from.id, '1')
