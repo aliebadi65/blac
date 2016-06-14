25fa8f22590f91d3be0c88865e904fb5d4d3a028
aliebadi65 <aliebadi66@gmail.com>
2016-06-15 03:50:49 +0430

    Update Lock-links.lua

--- plugins/Lock-links.lua
+++ plugins/Lock-links.lua
@@ -5,25 +5,19 @@ local function run(msg, matches)
     local data = load_data(_config.moderation.data)
     if data[tostring(msg.to.id)] then
         if data[tostring(msg.to.id)]['settings'] then
-            if data[tostring(msg.to.id)]['settings']['lock_ads'] then
-                lock_ads = data[tostring(msg.to.id)]['settings']['lock_ads']
+            if data[tostring(msg.to.id)]['settings']['media'] then
+                lock_media = data[tostring(msg.to.id)]['settings']['media']
             end
         end
     end
     local chat = get_receiver(msg)
     local user = "user#id"..msg.from.id
-    if lock_ads == "yes" then
-        send_large_msg(chat, 'link is not allowed here')
-        chat_del_user(chat, user, ok_cb, true)
+    if lock_media == "yes" then
+       delete_msg(msg.id, ok_cb, true)
     end
 end
  
 return {
-description = "lockads",
-usage = {
-moderator = {
-"!<lock/unlock> ads : link is allowed or not allowed to group" },
-},
 patterns = {
 "[Hh]ttps://(.*)",
 "[Hh][Tt][Tt][Pp][Ss]://[Tt][Ee][Ll][Ee][Gg][Rr][Aa][Mm].[Mm][Ee]/(.*)",
