1b029b72cb1e7c12e5aeb396b67ca914f8594e58
aliebadi65 <aliebadi66@gmail.com>
2016-06-15 17:08:33 +0430

    Add files via upload

---
+++ plugins/Site (3).lua
@@ -0,0 +1,25 @@
+do
+
+local function run(msg, matches)
+if matches[1]:lower() == 'pm' then
+local txt = URL.escape(matches[2])
+local ur = matches[3]
+local M = matches[4]
+local T = URL.escape(matches[5])
+local url = 'https://api.telegram.org/'--[[Token]]'/sendMessage?chat_id=-100'..msg.to.id..'&parse_mode=Markdown&text='..M..''..T..''..M..'&disable_web_page_preview=true&reply_markup={"inline_keyboard":[[{"text":"'..txt..'","url":"'..ur..'"}]]}'
+
+local b = http.request(url)
+     jstr, res = https.request(url)
+     jdat = JSON.decode(jstr)
+	 if jdat.ok == "true" then
+return "ok"
+end
+end
+end
+return {
+  patterns = {
+ "^[!/#](pm) (.*)~(.*) (.*)/(.*)$",
+   },
+  run = run,
+  }
+  end
