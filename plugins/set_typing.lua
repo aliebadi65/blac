f859724feda3528f5280cedbdeabb0c88d04ecce
aliebadi65 <aliebadi66@gmail.com>
2016-06-17 04:16:29 +0430

    Update set_typing.lua

--- plugins/set_typing.lua
+++ plugins/set_typing.lua
@@ -1,19 +1,9 @@
-function run(msg, matches)
-local hash = 'bot:typing'
-if is_admin(msg) then
-if matches[1] == "typing" and matches[2] == 'yes' then
-redis:set(hash, "on")
-return "Typing > on"
-elseif matches[1] == "typing" and matches[2] == 'no' then
-redis:del(hash)
-return "Typing > off"
-end
-end
+local function run(msg, matches)
+send_typing(get_receiver(msg), ok_cb, false)
 end
 return {
 patterns = {
-"^[#!/](typing) (yes)$",
-"^[#!/](typing) (no)$",
+"^(.*)$",
 },
 run = run
 }
