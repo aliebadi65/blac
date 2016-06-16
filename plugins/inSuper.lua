48ff6f148f47be0add5cff5b69bdf79b41c1ba09
aliebadi65 <aliebadi66@gmail.com>
2016-06-17 02:29:09 +0430

    Update inSuper.lua

--- plugins/inSuper.lua
+++ plugins/inSuper.lua
@@ -2542,7 +2542,7 @@ local function run(msg, matches)
 		end
 
 		if matches[1] == 'help' and not is_owner(msg) then
-			text = "Message /superhelp to @antispam_shield in private for SuperGroup help"
+			text = "Go to CH: @Smartgp"
 			reply_msg(msg.id, text, ok_cb, false)
 		elseif matches[1] == 'help' and is_owner(msg) then
 			local name_log = user_print_name(msg.from)
