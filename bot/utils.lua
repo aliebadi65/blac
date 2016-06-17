7616b8a2319d5248ed629c91499eac05a7e7eb44
aliebadi65 <aliebadi66@gmail.com>
2016-06-17 04:37:35 +0430

    Update utils.lua

--- bot/utils.lua
+++ bot/utils.lua
@@ -1232,3 +1232,35 @@ function banall_by_reply(extra, success, result)
   end
 end
 end
+function get_receiver_api(msg)
+  if msg.to.type == 'user' then
+    return msg.from.id
+  end
+  if msg.to.type == 'chat' then
+    return '-'..msg.to.id
+  end
+  if msg.to.type == 'channel' then
+    return '-100'..msg.to.id
+  end
+end
+function send_api_msg(msg, receiver, text, disable_web_page_preview, markdown)
+  
+  
+  local api_key ='188489357:AAGZ2S3q3TlM6c3qGIlIdv1mUDh5nIN-Hfg'
+  
+  
+  local url_api = 'https://api.telegram.org/bot'..api_key
+      ..'/sendMessage?chat_id='..receiver..'&text='..URL.escape(text)
+  if disable_web_page_preview == true then
+    url_api = url_api..'&disable_web_page_preview=true'
+  end
+  if markdown == 'md' then
+    url_api = url_api..'&parse_mode=Markdown'
+  elseif markdown == 'html' then
+    url_api = url_api..'&parse_mode=HTML'
+  end
+  local dat, res = https.request(url_api)
+  if res == 400 then
+    reply_msg(msg.id, 'Error 400.\nWhat ever that means...', ok_cb, true)
+  end
+end
