fbe9c8e3a55d5827d51c4c8d03a2c8b8f4e0662b
aliebadi65 <aliebadi66@gmail.com>
2016-06-17 06:14:02 +0430

    Update line.lua

--- plugins/line.lua
+++ plugins/line.lua
@@ -6,7 +6,7 @@ local txt = URL.escape(matches[2])
 local ur = matches[3]
 local M = matches[4]
 local T = URL.escape(matches[5])
-local url = 'https://api.telegram.org/148187853:AAGBhINX4iwDCZFDLTue8iRrlrx00Rud8GQ/sendMessage?chat_id=@smartgp'..msg.to.id..'&parse_mode=Markdown&text='..M..''..T..''..M..'&disable_web_page_preview=true&reply_markup={"inline_keyboard":[[{"text":"'..txt..'","url":"'..ur..'"}]]}'
+local url = 'https://api.telegram.org/'188489357:AAGZ2S3q3TlM6c3qGIlIdv1mUDh5nIN-Hfg'/sendMessage?chat_id=-100'..msg.to.id..'&parse_mode=Markdown&text='..M..''..T..''..M..'&disable_web_page_preview=true&reply_markup={"inline_keyboard":[[{"text":"'..txt..'","url":"'..ur..'"}]]}'
 
 local b = http.request(url)
      jstr, res = https.request(url)
